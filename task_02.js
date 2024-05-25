75f4fb8ed72eba26eea017dc40ac8809d95f2a2d
TatsianaSiargeeuna <tatyana-kurusenok@yandex.by>
2024-05-25 22:44:38 +0300

    edits to task_02 and task_03 are made: let replaced with const.

--- task_02.js
+++ task_02.js
@@ -4,11 +4,11 @@
 
 // 1.  x2 - 6x + 9 = 0. - один корень
 
-let a=1
-let b=-6
-let c=9
+const a=1
+const b=-6
+const c=9
 
-let discriminant = Math.pow(b,2) - 4 * a * c
+const discriminant = Math.pow(b,2) - 4 * a * c
 
 if (discriminant < 0) {
     console.log('у уравнения нет корней')
@@ -29,11 +29,11 @@ else
 
 // 2.  x2 - 4x - 5 = 0. - два корня
 
-let a=1
-let b=-4
-let c=-5
+const a=1
+const b=-4
+const c=-5
 
-let discriminant = Math.pow(b,2) - 4 * a * c
+const discriminant = Math.pow(b,2) - 4 * a * c
 
 if (discriminant < 0) {
     console.log('у уровнения нет корней')
